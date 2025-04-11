9a5ce21ef79e072eb4108a7d252648824397f30e
grenehom <grhom@umich.edu>
2025-04-11 17:02:13 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,64 @@
+var video;
+
+window.addEventListener("load", function () {
+  console.log("Good job opening the window");
+  video = document.getElementById("player1");
+  video.autoplay = false;
+  video.loop = false;
+  video.load();
+  console.log("Video source:", video.currentSrc);
+});
+
+document.querySelector("#play").addEventListener("click", function () {
+  console.log("Play Video");
+  video.play();
+  document.querySelector("#volume").innerText = video.volume * 100 + "%";
+  console.log("Play Video");
+});
+
+document.querySelector("#pause").addEventListener("click", function () {
+  video.pause();
+  console.log("Pause Video");
+});
+
+document.querySelector("#slower").addEventListener("click", function () {
+  video.playbackRate *= 0.9;
+  console.log("New speed is", video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function () {
+  video.playbackRate /= 0.9; // reverse of slower
+  console.log("New speed is", video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function () {
+  if (video.currentTime + 10 > video.duration) {
+    video.currentTime = 0;
+  } else {
+    video.currentTime += 10;
+  }
+  console.log("Current location is", video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function () {
+  if (video.muted) {
+    video.muted = false;
+    this.innerText = "Mute";
+  } else {
+    video.muted = true;
+    this.innerText = "Unmute";
+  }
+});
+
+document.querySelector("#slider").addEventListener("input", function () {
+  video.volume = this.value / 100;
+  document.querySelector("#volume").innerText = video.volume * 100 + "%";
+});
+
+document.querySelector("#vintage").addEventListener("click", function () {
+  video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function () {
+  video.classList.remove("oldSchool");
+});
